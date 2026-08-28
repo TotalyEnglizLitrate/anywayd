743cb1268b19eba320c592172bcb33637edec3ae
Narendra S <narendra.s1232@gmail.com>
2026-08-28 13:08:19 +0530

    enforce gil

--- src/anywayd/daemon/cli.py
+++ src/anywayd/daemon/cli.py
@@ -1,6 +1,8 @@
 import asyncio
 import logging
 import os
+import sys
+
 from typing import Annotated
 
 import typer
@@ -10,15 +12,23 @@ from rich.traceback import install
 from anywayd.daemon.service import service
 
 
+def _ensure_gil() -> None:
+    if hasattr(sys, "_is_gil_enabled"):
+        raise RuntimeError("anywayd does not support free-threaded Python builds")
+
+
 def _ensure_su():
     if os.geteuid() != 0:
         raise PermissionError("The anywayd daemon needs to be run as root.")
 
+
 app = typer.Typer()
+
 
 @app.command()
 def main(debug: Annotated[bool, typer.Option(help="Enable debug logging")] = False):
     _ = install(max_frames=3)
+    _ensure_gil()
     _ensure_su()
     asyncio.run(service(logging.DEBUG if debug else logging.INFO))
 

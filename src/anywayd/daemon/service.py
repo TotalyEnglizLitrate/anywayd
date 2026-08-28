fd2cf50e22f1c2a8aaa309a19dac1c72cce0a4d4
Narendra S <narendra.s1232@gmail.com>
2026-08-28 13:55:55 +0530

    split command into command + arguments to represent db state more accurately when returning Processes

--- src/anywayd/daemon/service.py
+++ src/anywayd/daemon/service.py
@@ -251,7 +251,8 @@ class AnywaydService(ServiceInterface):
         return {
             "uuid": Variant("s", str(proc.uuid)),
             "pid": Variant("x", proc.pid if proc.pid is not None else -1),
-            "command": Variant("s", f"{proc.command} {proc.arguments}"),
+            "command": Variant("s", proc.arguments),
+            "arguments": Variant("s", proc.arguments),
             "env": Variant("s", proc.env),
             "cwd": Variant("s", proc.cwd),
             "invoked_by_user": Variant("s", proc.invoked_by_user),
